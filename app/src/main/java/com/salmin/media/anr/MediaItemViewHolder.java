3b04ff2d92ba826bfcf7f65ee445e8ed3435387c
salminnella@gmail.com <salminnella@gmail.com>
2018-01-04 13:27:39 -0800

    added state fields to MediaItemViewHolder

--- app/src/main/java/com/salmin/media/anr/MediaItemViewHolder.java
+++ app/src/main/java/com/salmin/media/anr/MediaItemViewHolder.java
@@ -1,4 +1,11 @@
 package com.salmin.media.anr;
 
 public class MediaItemViewHolder {
+
+    static final int STATE_INVALID = -1;
+    static final int STATE_NONE = 0;
+    static final int STATE_PLAYABLE = 1;
+    static final int STATE_PAUSED = 2;
+    static final int STATE_PLAYING = 3;
+
 }
